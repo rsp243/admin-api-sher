eaf3bb91f542c7f9b4869d11e677f1424ad4835a
rsp243 <reshetov.semjon@gmail.com>
2021-01-07 14:16:21 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$.ajax({
+	type: 'GET',
+	url: "https://back-end-sher.herokuapp.com/api/v1/questions/?format=json",
+	dataType: 'json',  	
+  	success: function(data){
+    	alert("Прибыли данные: " + data);
+  	}
+});
+
+
+	//$('.icons img').each(function(){
+	//	if ($(this).attr('src') == 'img/icon3.png'){
+	//		$(this).fadeOut(1000);
+	//	}
+	//});
+
+	//function change(element, newAttr, newWalue){
+	//	var className = "." + element;
+	//	$(className).attr(newAttr, newWalue);
+	//}
+	//change("logo", "title", "Новая подсказка");
+
+	//$('.mainText').fadeTo(4000, 0.25);
+	//$('.mainText').slideUp(2000).slideDown(3000).fadeTo(2000, 1);
+
+	//$('.mainText').fadeTo(4000, 0.25).fadeTo(2000, 1);	
+
+	//function elemOut (element, time) {
+	//	if (time>5000 || time<1000){
+	//		return false;
+	//	} else {
+	//		var className = "." + element;
+	//		alert(className);
+	//		$(className).fadeOut(time);
+	//	}
+	//}
+	//elemOut('mainText', 5000);
+
+	//$('.mainText').fadeOut(2000).fadeIn(3000);
